8ffabf2f106f4ee5171e9e11b3b353855c4a93ca
leonardoasch <51801700+leonardoasch@users.noreply.github.com>
2021-04-26 02:54:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ def send_kafka(tempo,mongoid):
     y=json.dumps(x)
     producer.send(producer_topic, y.encode('utf-8'))    
 
-myclient = MongoClient("mongodb://10.0.10.4:27017/")
+myclient = MongoClient("mongodb://10.0.10.11:27017/")
 mydb = myclient["leonardo"]
 mycol = mydb["leonardostream"]
 
