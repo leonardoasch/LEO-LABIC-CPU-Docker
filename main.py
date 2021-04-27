f085fbc07c7fa73891ef4eaf1080c913b1b025f1
leonardoasch <51801700+leonardoasch@users.noreply.github.com>
2021-04-27 10:47:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ producer_topic = "leonardo-stream-2"
 
 consumer = KafkaConsumer(
      topic,
-     bootstrap_servers=['10.0.10.1:9092'],
+     bootstrap_servers=['10.0.10.11:9092'],
      auto_offset_reset='earliest',
      enable_auto_commit=True,
      group_id='my-group',
@@ -35,7 +35,7 @@ consumer = KafkaConsumer(
 
      
 # Start up producer
-producer = KafkaProducer(bootstrap_servers='10.0.10.1:9092',
+producer = KafkaProducer(bootstrap_servers='10.0.10.11:9092',
 compression_type='gzip',
 linger_ms=5
 )
