4b48455129f923d686b279cbe9e9a5214d22630b
leonardoasch <51801700+leonardoasch@users.noreply.github.com>
2021-06-12 16:22:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -64,46 +64,6 @@ def stringToRGB(base64_string):
     image = Image.open(BytesIO(imgdata))
     return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)   
 
-def nms(detections, threshold=.5):
-   
-    detections = sorted(detections, key=lambda detections: detections[2],
-            reverse=True)
-    
-    new_detections=[]
-    
-    new_detections.append(detections[0])
-    
-    del detections[0]
-    
-    for index, detection in enumerate(detections):
-        for new_detection in new_detections:
-            if overlapping_area(detection, new_detection) > threshold:
-                del detections[index]
-                break
-        else:
-            new_detections.append(detection)
-            del detections[index]
-    return new_detections
-
-def overlapping_area(detection_1, detection_2):
-    
-    x1_tl = detection_1[0]
-    x2_tl = detection_2[0]
-    x1_br = detection_1[0] + detection_1[3]
-    x2_br = detection_2[0] + detection_2[3]
-    y1_tl = detection_1[1]
-    y2_tl = detection_2[1]
-    y1_br = detection_1[1] + detection_1[4]
-    y2_br = detection_2[1] + detection_2[4]
-    # Calculate the overlapping Area
-    x_overlap = max(0, min(x1_br, x2_br)-max(x1_tl, x2_tl))
-    y_overlap = max(0, min(y1_br, y2_br)-max(y1_tl, y2_tl))
-    overlap_area = x_overlap * y_overlap
-    area_1 = detection_1[3] * detection_2[4]
-    area_2 = detection_2[3] * detection_2[4]
-    total_area = area_1 + area_2 - overlap_area
-    return overlap_area / float(total_area)
-    
 
 def correct_encoding(dictionary):
     """Correct the encoding of python dictionaries so they can be encoded to mongodb
